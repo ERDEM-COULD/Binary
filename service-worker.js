d5efed0c81163a8de7d4d329fb4fb555ee9201ee
ERDEM-COULD <merdem58171.3@gmail.com>
2025-06-06 12:31:52 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,25 @@
-const CACHE_NAME = 'ceviri-app-cache-v1';
+const CACHE_NAME = 'erdem-cache-v1';
 const urlsToCache = [
-  '.',
-  'index.html',
-  'icon-192.png',
-  'icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-// Install event - cache dosyaları yükle
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// Activate event - eski cache'leri temizle
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => 
-      Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
-    )
-  );
-});
-
-// Fetch event - önce cache, sonra ağdan yükle
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => 
-      response || fetch(event.request)
-    )
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
+
